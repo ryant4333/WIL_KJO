365713f4147373ab8e453a01d6c6ca793490fcd9
Spedcake <charrison125@hotmail.com>
2020-08-17 12:33:28 +1000

    added zdt file

--- src/Particle.py
+++ src/Particle.py
@@ -1,33 +1,22 @@
 import numpy as np
+import zdt_test as zdt
 
 
-class particle:
-    def __init__(self, dims, min, max):     # init particle attributes
-        self.position = np.random.uniform(low=min, high=max, size=dims)
-        self.position_fitness = problem(self.position)
-        self.velocity = np.random.uniform(low=min, high=max, size=dims)
+class Particle:
+    def __init__(self, dims, min_, max_):     # init particle attributes
+        self.position = np.random.uniform(low=min_, high=max_, size=dims)
+        self.position_fitness = zdt.ZDT1(self.position)
+        self.velocity = np.random.uniform(low=min_, high=max_, size=dims)
 
         self.sBest_position = self.position.copy()
         self.pBest_position = self.position.copy()
-        self.pBest_fitness = problem(self.position)
+        self.pBest_fitness = self.position_fitness.copy()
 
-    def evaluate(self,pos):
+    def evaluate(self, pos):
         self.position = pos
-        self.position_fitness = problem(pos)
+        self.position_fitness = zdt.ZDT1(pos)
 
         if self.position_fitness < self.pBest_fitness:
             self.pBest_position = self.position
             self.pBest_fitness = self.position_fitness
 
-    def moveTo(self):
-        optimise(self.position)
-
-
-
-
-
-class problem:
-    "Feed the particle position through to find fitness"
-
-class optimise:
-    "Moving the particle around the testspace"
