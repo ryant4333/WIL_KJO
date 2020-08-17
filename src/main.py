2cc7f16809c2c70eda72eb6bc54c550601bf7aa7
Lucas Geurtjens <lucas.geurtjens@griffithuni.edu.au>
2020-08-17 13:16:01 +1000

    cleaning up file

--- src/main.py
+++ src/main.py
@@ -52,9 +52,6 @@ class Swarm:
     """
     Swarm class
     """
-    def apples(self):
-        return
-    test = "Hello"
 
 
 class Optimiser:
@@ -67,20 +64,19 @@ class Archive:
     """
     Archive class
     """
-    a = 1
+
 
 class Solution:
     """
     Solution class
     """
-    test = 0
+
 
 def main():
     print("Main here")
     problem = Problem('config.json')
     print(problem.c1)
     return
-
 
 
 if __name__ == '__main__':
