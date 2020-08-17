3ba67511c284302967dffdd9f0007ef02b0b2597
Ryan Taylor <RyanCTaylor95@gmail.com>
2020-08-17 10:24:55 +1000

    zdt3&4

--- zdt_test.py
+++ zdt_test.py
@@ -27,11 +27,23 @@ def ZDT3(x):
 
     return [f1, f2]
 
+def ZDT4(x):
+    #x[0] = between 0-1
+    #x[1:D] = between -5 to 5
+    #D = 2-10
+    D = len(x)
+    f1 = x[0]
+    g = 1 + 10*(D-1) + np.sum(x[1:D]**2 - 10*np.cos(4*np.pi*x[1:D]))
+    h = 1 - np.sqrt(f1/g)
+    f2 = g * h
 
+    return [f1, f2]
 
 parts = 30
 x = np.random.rand(parts)
 print(x)
 
-objectiveValues = ZDT3(x)
+objectiveValues = [ZDT1(x), ZDT2(x), ZDT3(x)]
 print(objectiveValues)
+
+#ZDT4 has to have different initial values
