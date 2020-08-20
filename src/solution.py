089f13550d15b7d46703a9bec629d0f13d33d5da
jaydenthomson1999 <jayden.thomson@griffithuni.edu.au>
2020-08-20 12:01:14 +1000

    fixed syntax of solution

--- src/solution.py
+++ src/solution.py
@@ -1,13 +1,14 @@
 class Solution:
-	def __init__(self, x, objectives):
+    def __init__(self, x, objectives):
         self.x = x
         self.objectives = objectives
+
     def fully_dominated(self, solution, optimization_type):
         for i in range(len(self.objectives)):
-            if(optimization_type[i] == "MAX"):
-                if(self.objectives[i] > solution.objectives[i]):
+            if optimization_type[i] == "MAX":
+                if self.objectives[i] > solution.objectives[i]:
                     return False
-            else if(optimization_type[i] == "MIN"):
-                if(self.objectives[i] < solution.objectives[i]):
+            elif optimization_type[i] == "MIN":
+                if self.objectives[i] < solution.objectives[i]:
                     return False
         return True
