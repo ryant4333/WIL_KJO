10ab3dd070a241e7e55152de739df487d407872b
Spedcake <charrison125@Hotmail.com>
2020-08-20 19:27:38 +1000

    Remade the swarm.py to fit specification

--- src/swarm.py
+++ src/swarm.py
@@ -3,28 +3,30 @@ import numpy as np
 import main as main
 
 config = main.Problem("config.json")
-# min = (0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0)
-# max = (1, 2, 3, 1, 1, 1, 1, 1, 1, 1, 1)
-# num_particles = 3
+arr_list = []
+eval_counter = 0    #TODO: Not really sure what to do with this yet.
 
 
 class Swarm:
 
     def __init__(self, num_particles):
-        self.swarm = [particle.Particle(p_values()) for _ in range(num_particles)]
+        self.swarm = [particle.Particle() for _ in range(num_particles)]
+        set_x(self)
 
 
-def p_values():
-    x = []
+def set_x(self):
     for i in range(len(config.min)):
-        x.append(np.random.uniform(config.min[i], config.max[i]))
+        arr_list.append(np.random.uniform(config.min[i], config.max[i], config.particle_num))
+    np_arr = np.array(arr_list)
 
-    np_array = np.array(x)
-    return np_array
+    for j in range(config.particle_num):
+        np.random.shuffle(np_arr[j])
+        x_pos = np_arr[:, j]
+        self.swarm[j].x = x_pos
 
 
 if __name__ == "__main__":
 
     test_swarm = Swarm(config.particle_num)
-    for i in test_swarm.swarm:
-        print(i.x)
+    for i in range(len(test_swarm.swarm)):
+        print(test_swarm.swarm[i].x)
