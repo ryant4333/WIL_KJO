e626066d9f023d39305bde5cfc346cb82b3963fe
Spedcake <charrison125@Hotmail.com>
2020-08-20 12:06:41 +1000

    Fixed some errors in the particle.py

--- src/particle.py
+++ src/particle.py
@@ -1,24 +1,26 @@
+import solution as solutions
+
+
 class Particle:
 
-  def __init__(self):
-      self.p_best = None
-      self.s_best = None
-      self.x = None
-      self.velocity = None
+    def __init__(self):
+        self.p_best = None
+        self.s_best = None
+        self.x = None
+        self.velocity = None
 
-
-  def evaluate(self, objectives, optimization_type):
-      """
+    def evaluate(self, objectives, optimization_type):
+        """
       maybe updates p_best
       and returns new solution
       """
-      obj = objectives(self.x)
-      solution = Solution(self.x, obj)
-      
-      if(self.p_best.fully_dominated(solution, optimization_type) == True):
-      	self.p_best = solution
-      
-      return solution
-  
-  def move(self, c1, c2, w, maximum, minimum):
-    pass
+        obj = objectives(self.x)
+        solution = solutions.Solution(self.x, obj)
+
+        if self.p_best.fully_dominated(solution, optimization_type):
+            self.p_best = solution
+
+        return solution
+
+    def move(self, c1, c2, w, maximum, minimum):
+        pass
