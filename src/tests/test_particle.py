1079d16e300b2de54b3b4bf074752430a6c09fdf
jaydenthomson1999 <jayden.thomson@griffithuni.edu.au>
2020-08-21 15:23:12 +1000

    finished particle test, and chnaged move function error message

--- src/tests/test_particle.py
+++ src/tests/test_particle.py
@@ -8,6 +8,8 @@ from particle import Particle
 
 class TestParticle(unittest.TestCase):
 
+    # tests that particle returns a solution with the correct fields and doesnt 
+    # update pbest
     def test_particle_evaluate_no_pbest_update(self):
         particle = Particle()
         particle.x = [1, 1]
@@ -15,9 +17,12 @@ class TestParticle(unittest.TestCase):
         sol = particle.evaluate(basicObjective, ["MAX", "MAX"])
         self.assertEqual(sol.objectives[0], 2)
         self.assertEqual(sol.objectives[1], 2)
+        self.assertEqual(sol.x[0], 1)
+        self.assertEqual(sol.x[1], 1)
         self.assertEqual(particle.p_best.objectives[0], 5)
         self.assertEqual(particle.p_best.objectives[1], 5)
     
+    # tests that the pbest is updated with fully dominated solution is found
     def test_particle_evaluate_with_pbest_update(self):
         particle = Particle()
         particle.x = [5, 5]
@@ -27,6 +32,18 @@ class TestParticle(unittest.TestCase):
         self.assertEqual(sol.objectives[1], 6)
         self.assertEqual(particle.p_best.objectives[0], 6)
         self.assertEqual(particle.p_best.objectives[1], 6)
+    
+    # tests that the max or the min param in move is incorrect
+    def test_particle_move_type_error(self):
+        particle = Particle()
+        particle.p_best = Solution([1, 1], [1, 1])
+        particle.s_best = Solution([1, 1], [5, 5])
+        particle.x = [0, 0]
+        particle.velocity = [0.5, 0.5]
+        with self.assertRaises(TypeError):
+            particle.move(1,1,1, [100,100], [-100])
+        with self.assertRaises(TypeError):
+            particle.move(1,1,1, [100], [-100,-100])
 
 def basicObjective(x):
     new_x = []
