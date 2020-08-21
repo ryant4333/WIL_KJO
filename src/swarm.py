3dadc9a73ccfe05f89b8b3cf28b0b366bad8d26b
Spedcake <charrison125@Hotmail.com>
2020-08-21 21:42:39 +1000

    Cleaned up code in swarm.py

--- src/swarm.py
+++ src/swarm.py
@@ -8,25 +8,19 @@ class Swarm:
         self.swarm = [particle.Particle() for _ in range(num_particles)]
         self.eval_count = 0
 
+        # Create numpy array for particle positions and velocity
+        np_pos = np.array([np.random.uniform(min_[x], max_[x], num_particles) for x in range(len(min_))])
         np_vel = np.random.uniform(0, 0, len(min_))
-        list_pos = []
 
-        for i in range(len(min_)):
-            list_pos.append(np.random.uniform(min_[i], max_[i], num_particles))
-
-        np_arr = np.array(list_pos)
-
-        for x in range(len(min_)):
-            np.random.shuffle(np_arr[x, :])
+        # Assign a position and velocity to each particle in swarm
         for j in range(num_particles):
-            x_pos = np_arr[:, j]
+            x_pos = np_pos[:, j]
             self.swarm[j].x = x_pos
             self.swarm[j].velocity = np.array(np_vel)
 
 
 if __name__ == "__main__":
-    test_swarm = Swarm(13, [0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0], [1, 2, 3, 1, 1, 1, 1, 1, 1, 1, 1])
-    test_swarm.swarm[0].x[0] = 1.1
+    test_swarm = Swarm(13, [0, 1, 2, 0, 0], [1, 2, 3, 1, 1])
 
     for i in range(len(test_swarm.swarm)):
         print(test_swarm.swarm[i].x)
