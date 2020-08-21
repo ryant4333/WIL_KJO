bdec19623fad546f10f3876615fa25ce03051b62
Spedcake <charrison125@Hotmail.com>
2020-08-21 16:18:01 +1000

    Added test cases for swarm.py

--- src/swarm.py
+++ src/swarm.py
@@ -7,21 +7,26 @@ class Swarm:
     def __init__(self, num_particles, min_, max_):
         self.swarm = [particle.Particle() for _ in range(num_particles)]
         self.eval_count = 0
-        arr_list = []
+
+        np_vel = np.random.uniform(0, 0, len(min_))
+        list_pos = []
 
         for i in range(len(min_)):
-            arr_list.append(np.random.uniform(min_[i], max_[i], num_particles))
+            list_pos.append(np.random.uniform(min_[i], max_[i], num_particles))
 
-        np_arr = np.array(arr_list)
+        np_arr = np.array(list_pos)
 
         for x in range(len(min_)):
-            np.random.shuffle(np_arr[x,:])
+            np.random.shuffle(np_arr[x, :])
         for j in range(num_particles):
             x_pos = np_arr[:, j]
             self.swarm[j].x = x_pos
+            self.swarm[j].velocity = np.array(np_vel)
 
 
 if __name__ == "__main__":
     test_swarm = Swarm(13, [0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0], [1, 2, 3, 1, 1, 1, 1, 1, 1, 1, 1])
+    test_swarm.swarm[0].x[0] = 1.1
+
     for i in range(len(test_swarm.swarm)):
         print(test_swarm.swarm[i].x)
--- src/tests/test_swarm.py
+++ src/tests/test_swarm.py
@@ -1,18 +1,35 @@
-from unittest import TestCase
+# needs to run from test file
+import unittest
+import sys
+from swarm import Swarm
+import numpy as np
+sys.path.insert(1, "../")  # to import running in the test path
 
-import swarm
-import problem
+
+class TestSwarm(unittest.TestCase):
+
+    def test_num_particles_swarm(self):
+        test_swarm = Swarm(num_particles=13, min_=[0, 1, 2], max_=[1, 2, 3])
+        num_p = len(test_swarm.swarm) == 13
+        self.assertEqual(num_p, True)
+
+    def test_num_dims_particles(self):
+        test_swarm = Swarm(num_particles=13, min_=[0, 1, 2], max_=[1, 2, 3])
+        dims_p = all(len(x.x) == 3 for x in test_swarm.swarm)
+        self.assertEqual(dims_p, True)
+
+    def test_range_particle_dims(self):
+        test_swarm = Swarm(num_particles=13, min_=[0, 1, 2], max_=[1, 2, 3])
+        high = [1, 2, 3]
+        low = [0, 1, 2]
+        range_dims = all(high[y] >= x.x[y] >= low[y] for x, y in zip(test_swarm.swarm, range(len(test_swarm.swarm[0].x))))
+        self.assertEqual(range_dims, True)
+
+    def test_num_dims_velocity(self):
+        test_swarm = Swarm(num_particles=13, min_=[0, 1, 2], max_=[1, 2, 3])
+        vel_p = all(len(x.velocity) == 3 for x in test_swarm.swarm)
+        self.assertEqual(vel_p, True)
 
 
-class TestSolutionDomination(TestCase):
-
-    def setUp(self):
-        self.config = problem.Problem()
-        self.swarm = swarm.Swarm(self.config.particle_num, True)
-
-    def tearDown(self):
-        self.config = None
-        self.swarm = None
-
-    def test_set_x(self):  # TODO: set x test
-        pass
+if __name__ == "__main__":
+    unittest.main()
