f22d192ad360009e4908c8668460b61436743cb3
jaydenthomson1999 <jayden.thomson@griffithuni.edu.au>
2020-08-21 14:49:00 +1000

    done evaluate test functions for particle class, not sure how to do move tests

--- src/tests/test_particle.py
+++ src/tests/test_particle.py
@@ -1,18 +1,38 @@
-from unittest import TestCase
+# needs to run from test file
+import unittest
+import sys
+sys.path.insert(1, "../") # to import py classes
+from solution import Solution
+from particle import Particle
 
-import particle
 
+class TestParticle(unittest.TestCase):
 
-class TestSolutionDomination(TestCase):
+    def test_particle_evaluate_no_pbest_update(self):
+        particle = Particle()
+        particle.x = [1, 1]
+        particle.p_best = Solution([4,4], [5, 5])
+        sol = particle.evaluate(basicObjective, ["MAX", "MAX"])
+        self.assertEqual(sol.objectives[0], 2)
+        self.assertEqual(sol.objectives[1], 2)
+        self.assertEqual(particle.p_best.objectives[0], 5)
+        self.assertEqual(particle.p_best.objectives[1], 5)
+    
+    def test_particle_evaluate_with_pbest_update(self):
+        particle = Particle()
+        particle.x = [5, 5]
+        particle.p_best = Solution([4,4], [5, 5])
+        sol = particle.evaluate(basicObjective, ["MAX", "MAX"])
+        self.assertEqual(sol.objectives[0], 6)
+        self.assertEqual(sol.objectives[1], 6)
+        self.assertEqual(particle.p_best.objectives[0], 6)
+        self.assertEqual(particle.p_best.objectives[1], 6)
 
-    def setUp(self):
-        self.particle = particle.Particle()
+def basicObjective(x):
+    new_x = []
+    for i in range(len(x)):
+        new_x.append(x[i] + 1)
+    return new_x
 
-    def tearDown(self):
-        self.particle = None
-
-    def test_particle_evaluate(self):  # TODO: Particle evaluating test
-        pass
-
-    def test_particle_move(self):  # TODO: Particle moving test
-        pass
+if __name__ == "__main__":
+    unittest.main()
