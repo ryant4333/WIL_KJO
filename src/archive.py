b9908049c316df6501e537e0ff17abd5ca11995f
Ryan Taylor <RyanCTaylor95@gmail.com>
2020-08-21 11:37:51 +1000

    Add Archive file init

---
+++ src/archive.py
@@ -0,0 +1,12 @@
+import linked list thing
+
+"""
+start with empty linked list
+Define push function particles can use to push evaluated position
+Use fully_dominated function from solution to compare pushed result against archive
+trim fully dominated solutions
+Output function to return list at end
+"""
+class Archive:
+    def __init__(self):
+        
