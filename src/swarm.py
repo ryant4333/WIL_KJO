dfc74091d105bd912a4ad78ef1b22982a06f8d10
jaydenthomson1999 <jayden.thomson@griffithuni.edu.au>
2020-08-21 11:34:06 +1000

    removed unwanted class attributes

--- src/swarm.py
+++ src/swarm.py
@@ -7,17 +7,17 @@ class Swarm:
     def __init__(self, num_particles, min_, max_):
         self.swarm = [particle.Particle() for _ in range(num_particles)]
         self.eval_count = 0
-        self.arr_list = []
+        arr_list = []
 
         for i in range(len(min_)):
-            self.arr_list.append(np.random.uniform(min_[i], max_[i], num_particles))
+            arr_list.append(np.random.uniform(min_[i], max_[i], num_particles))
 
-        self.np_arr = np.array(self.arr_list)
+        np_arr = np.array(arr_list)
 
         for x in range(len(min_)):
-            np.random.shuffle(self.np_arr[x,:])
+            np.random.shuffle(np_arr[x,:])
         for j in range(num_particles):
-            x_pos = self.np_arr[:, j]
+            x_pos = np_arr[:, j]
             self.swarm[j].x = x_pos
 
 
