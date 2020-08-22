a8787d40c46ee5414493825ad313a767c12ff9e2
Ryan Taylor <RyanCTaylor95@gmail.com>
2020-08-22 11:33:53 +1000

    Add push function with updated dom function

--- src/archive.py
+++ src/archive.py
@@ -42,9 +42,31 @@ class Archive:
                 ll_pos -= 1
             ll_pos += 1
         print("Adding solution to list: ", new_solution)
-        self.archive.append(new_solution)    
+        self.archive.append(new_solution)   
 
+    def push2(self, new_solution, optimization_type):
+        ll_pos = 0
 
+        for sol in self.archive:
+            dom_status = new_solution.dominated(sol, optimization_type)
+
+            if dom_status == -1:
+                #Self/new_solution is dominated, end
+                return
+            elif dom_status == 1:
+                #Archive sol is dominated by new_solution
+                node = self.archive.nodeat(ll_pos)
+                self.archive.remove(node)
+                ll_pos -= 1
+                #As archive is shorter pos needs to be shorter
+            elif dom_status == 0:
+                pass
+            else:
+                raise ValueError("Dom status has incorrect value:", dom_status)
+            ll_pos += 1
+        
+        self.archive.append(new_solution)
+        return
 
 
     def output(self):
@@ -59,10 +81,10 @@ if __name__ == "__main__":
 
     arch = Archive()
 
-    arch.push(sol1)
-    arch.push(sol2)
-    arch.push(sol3)
-    arch.push(sol4)
-    arch.push(sol5)
+    arch.push2(sol1, ["MAX", "MAX"])
+    arch.push2(sol2, ["MAX", "MAX"])
+    arch.push2(sol3, ["MAX", "MAX"])
+    arch.push2(sol4, ["MAX", "MAX"])
+    arch.push2(sol5, ["MAX", "MAX"])
 
     print(arch.output())
