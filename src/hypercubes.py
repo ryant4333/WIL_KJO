c53afde74834963e817d2c4368fe659d27787b50
Ryan Taylor <RyanCTaylor95@gmail.com>
2020-08-26 15:31:19 +1000

    Add bin key and add sol functions

--- src/hypercubes.py
+++ src/hypercubes.py
@@ -1,3 +1,49 @@
+from zdt_test import ZDT1
+from solution import Solution
+import numpy as np
+
 class Hypercubes:
-    def __init__(self, d_min, d_max, objectives, cube_count):
-        pass
+    def __init__(self, d_min, d_max, objective, cube_count):
+
+        self.cube_dict = {}
+
+        s_min = objective(d_min)
+        s_max = objective(d_max)
+        
+        key_dist = []
+        self.bin_size = []
+        for i in range(len(s_min)):
+            key_dist.append(s_max[i] - s_min[i])
+            self.bin_size.append(key_dist[i] / cube_count)
+
+    def get_bin_key(self, sol):
+        """
+        input is solution object
+        """
+        key_list = []
+        for i in range(len(sol.objectives)):
+            key_val = int(sol.objectives[i] / self.bin_size[i])
+            key_list.append(key_val)
+        return str(key_list)
+
+    def add_sol(self, sol):
+        #What if they exactly the same
+        key = get_bin_key(sol)
+        if key in self.cube_dict:
+            self.cube_dict[key].append(sol)
+        else:
+            self.cube_dict[key] = [sol]
+
+        
+
+
+if __name__ == "__main__":
+    mini = np.array([0,0,0,0,0,0,0,0])
+    maxi = np.array([1,1,1,1,1,1,1,1])
+    
+    sol1 = Solution([], [.5, 3.4])
+    # sol2 = Solution([], [.6, 3.1])
+    
+    hype = Hypercubes(mini, maxi, ZDT1, 5)
+    print(hype.get_bin_key(sol1))
+    
