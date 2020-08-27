a4fc6c8614a1009e6bae7efb9e3d4d26ab347c1b
Spedcake <charrison125@hotmail.com>
2020-08-27 13:05:22 +1000

    now grabs title, xrange, yrange from input

--- src/plot_graph.py
+++ src/plot_graph.py
@@ -1,23 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
-file = open('pareto_front').read().splitlines()
-front = np.loadtxt('pareto_front')
 
 
-def plot():
-
+def plot(title, x_range, y_range):
+    front = np.loadtxt('front_output')
     plt.scatter(front[:, 0], front[:, 1], c='b')
-    plt.title(r'ZDT1 Search Domain: $0\leq x_i\leq1,1\leq i\leq30.\mathrm{}$')
+    plt.title(title)
     plt.xlabel(r'$f_1(x)$')
     plt.ylabel(r'$f_2(x)$')
-    plt.legend(["Pareto front"])
-    plt.ylim(0,10)
-    plt.xlim(0,10)
+    plt.legend(["Front"])
+    plt.ylim(y_range)
+    plt.xlim(x_range)
     plt.show()
 
 
-
 if __name__ == "__main__":
-    plot()
+
+    plot(r'ZDT1 Search Domain: $0\leq x_i\leq1,1\leq i\leq30.\mathrm{}$', [0, 11], [0, 12])
 
 
