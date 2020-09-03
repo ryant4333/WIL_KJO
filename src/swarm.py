97362f23205bb9ad81b25b6c93ac1d7e564f179c
Spedcake <charrison125@Hotmail.com>
2020-09-03 17:16:40 +1000

    Adjustments to swarm.py

--- src/swarm.py
+++ src/swarm.py
@@ -7,25 +7,23 @@ class Swarm:
     def __init__(self, num_particles, min_, max_, even):
         self.particles = [particle.Particle() for _ in range(num_particles)]
         self.eval_count = 0
+        np_vel = np.random.uniform(0, 0, len(min_))
 
         # Even dist particles
         arr_list = []
         for i in range(len(max_)):
             dist = (max_[i] - min_[i]) / num_particles
             arr_list.append(np.arange(min_[i], max_[i], dist))
-        test = np.array(arr_list)
-        np.random.shuffle((test[0]))
+        arr_sorted = np.array(arr_list)
+        np.random.shuffle((arr_sorted[0]))
 
         # Random dist particles
         np_pos = np.array([np.random.uniform(min_[x], max_[x], num_particles) for x in range(len(min_))])
 
-
-        np_vel = np.random.uniform(0, 0, len(min_))
-
         # Assign a position and velocity to each particle in swarm
         for j in range(num_particles):
             if even:
-                x_pos = test[:, j]
+                x_pos = arr_sorted[:, j]
             else:
                 x_pos = np_pos[:, j]
             self.particles[j].x = x_pos
