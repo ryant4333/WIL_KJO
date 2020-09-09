ab8e1d555b6cb1587aa123e78b462869c5194fa9
Lucas Geurtjens <lucas.geurtjens@griffithuni.edu.au>
2020-09-09 14:28:18 +1000

    Tweaking bugs in the Problem tests

--- src/tests/test_problem.py
+++ src/tests/test_problem.py
@@ -15,81 +15,81 @@ class TestProblem(unittest.TestCase):
     def tearDown(self):
         self.problem = None
 
-    def test_import_zdt(self):
+    def test_convert_to_method(self):
         test_input = np.array([0.1, 1])
         self.assertEqual(self.problem.objective(test_input), zdt_test.ZDT1(test_input))
 
-    def test_validation_c1_type(self):
+    def test_c1_type(self):
         self.problem.c1 = "1.1"
         with self.assertRaises(TypeError):
             self.problem.validate_config()
 
-    def test_validation_c2_type(self):
+    def test_c2_type(self):
         self.problem.c2 = [1]
         with self.assertRaises(TypeError):
             self.problem.validate_config()
 
-    def test_validation_min_w_type(self):
+    def test_min_w_type(self):
         self.problem.min_w = "1.1"
         with self.assertRaises(TypeError):
             self.problem.validate_config()
 
-    def test_validation_max_w_type(self):
+    def test_max_w_type(self):
         self.problem.max_w = "1.1"
         with self.assertRaises(TypeError):
             self.problem.validate_config()
 
-    def test_validation_particle_num_type(self):
+    def test_particle_num_type(self):
         self.problem.particle_num = 1.1
         with self.assertRaises(TypeError):
             self.problem.validate_config()
 
-    def test_validation_min_avg_velocity_type(self):
-        self.problem.particle_num = "1.1"
+    def test_min_avg_velocity_type(self):
+        self.problem.min_avg_velocity = "1.1"
         with self.assertRaises(TypeError):
             self.problem.validate_config()
 
-    def test_validation_max_type(self):
+    def test_max_type(self):
         self.problem.max = 1
         with self.assertRaises(TypeError):
             self.problem.validate_config()
 
-    def test_validation_max_nested_type(self):
+    def test_max_nested_type(self):
         self.problem.max = [1, "1", 1]
         with self.assertRaises(TypeError):
             self.problem.validate_config()
 
-    def test_validation_min_type(self):
-        self.problem.max = 1
+    def test_min_type(self):
+        self.problem.min = 1
         with self.assertRaises(TypeError):
             self.problem.validate_config()
 
-    def test_validation_min_nested_type(self):
+    def test_min_nested_type(self):
         self.problem.min = [1, "1", 1]
         with self.assertRaises(TypeError):
             self.problem.validate_config()
 
-    def test_validation_optimization_type_type(self):
+    def test_optimization_type_type(self):
         self.problem.optimization_type = 1
         with self.assertRaises(TypeError):
             self.problem.validate_config()
 
-    def test_validation_optimization_type_nested_type(self):
+    def test_optimization_type_nested_type(self):
         self.problem.optimization_type = [1, 1]
         with self.assertRaises(TypeError):
             self.problem.validate_config()
 
-    def test_validation_cube_count_type(self):
+    def test_cube_count_type(self):
         self.problem.cube_count = 1.1
         with self.assertRaises(TypeError):
             self.problem.validate_config()
 
-    def test_validation_solution_count_type(self):
+    def test_solution_count_type(self):
         self.problem.solution_count = 1.1
         with self.assertRaises(TypeError):
             self.problem.validate_config()
 
-    def test_validation_particle_num_value(self):
+    def test_particle_num_value(self):
         self.problem.particle_num = 0
         with self.assertRaises(ValueError):
             self.problem.validate_config()
@@ -137,6 +137,7 @@ class TestProblem(unittest.TestCase):
         self.problem.swarm_distribution = "MEH"
         with self.assertRaises(ValueError):
             self.problem.validate_values()
+
 
 if __name__ == "__main__":
     unittest.main()
