b257d39ce9f206ca96ac7b6d391379b4589f4be5
jaydenThomson1999 <jaydenthomson1999@gmail.com>
2020-09-25 11:15:07 +1000

    commented schduler

--- scheduler.sh
+++ scheduler.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-#PBS -M ryan.taylor5@griffithuni.edu.au
-#PBS -N KJO1
+user="ryan.taylor5@griffithuni.edu.au"
+problem_folder="./benchmarks/zdt/zdt3"
+griffith_number="s2896663"
+#PBS -M $user
+#PBS -N KJO
 #PBS -q workq
-#PBS -l select=1:ncpus=10:mem=2gb,walltime=0:10:00
+#PBS -l select=1:ncpus=1:mem=2gb,walltime=0:10:00
 
 cd $PBS_O_WORKDIR
-cd /export/home/s2896663/WIL_KJO2/
+cd /export/home/$griffith_number/WIL_KJO/
 module load anaconda3/2019.07py3
+source /usr/local/bin/s3proxy.sh
 source activate 3point6
+pip install pymop
 
-python ./optimiser.py ./benchmarks/zdt/zdt3
+python ./optimiser.py $problem_folder
